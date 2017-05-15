ebe416b992781f17bee906f0dd06b4082001396c
Luis Miguel Flores Monzón <luis.flores@dextratech.com>
2017-05-15 11:59:23 -0500

    Initial commit Add Image Resource buttons for Video and Camera

---
+++ app/src/main/java/com/example/luisflores/camara2api/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.luisflores.camara2api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
